b61f01c3907e080946988960fd8dab90b971943f
Quantum0 <quantum0@mail.ru>
2016-08-12 06:42:02 +0300

    Добавление "новой игры" "загрузки" и "сохранения" в интерфейс и изменение способа отображения (не доделано)

--- FormSingle.Designer.cs
+++ FormSingle.Designer.cs
@@ -29,9 +29,12 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.labelCurrentTurn = new System.Windows.Forms.Label();
+            this.buttonSettings = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonNewGame = new System.Windows.Forms.Button();
+            this.buttonLoadGame = new System.Windows.Forms.Button();
+            this.buttonSaveGame = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -44,45 +47,86 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Текущий ход: ";
             // 
-            // label2
+            // labelCurrentTurn
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(93, 9);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.labelCurrentTurn.AutoSize = true;
+            this.labelCurrentTurn.Location = new System.Drawing.Point(93, 9);
+            this.labelCurrentTurn.Name = "labelCurrentTurn";
+            this.labelCurrentTurn.Size = new System.Drawing.Size(10, 13);
+            this.labelCurrentTurn.TabIndex = 2;
+            this.labelCurrentTurn.Text = "-";
             // 
-            // button1
+            // buttonSettings
             // 
-            this.button1.Location = new System.Drawing.Point(197, 5);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Настройки";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonSettings.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonSettings.Location = new System.Drawing.Point(255, 226);
+            this.buttonSettings.Name = "buttonSettings";
+            this.buttonSettings.Size = new System.Drawing.Size(75, 23);
+            this.buttonSettings.TabIndex = 0;
+            this.buttonSettings.Text = "Настройки";
+            this.buttonSettings.UseVisualStyleBackColor = true;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.pictureBox1.Location = new System.Drawing.Point(12, 34);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(260, 215);
+            this.pictureBox1.Size = new System.Drawing.Size(316, 176);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // buttonNewGame
+            // 
+            this.buttonNewGame.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonNewGame.Location = new System.Drawing.Point(12, 226);
+            this.buttonNewGame.Name = "buttonNewGame";
+            this.buttonNewGame.Size = new System.Drawing.Size(75, 23);
+            this.buttonNewGame.TabIndex = 4;
+            this.buttonNewGame.Text = "Новая игра";
+            this.buttonNewGame.UseVisualStyleBackColor = true;
+            this.buttonNewGame.Click += new System.EventHandler(this.buttonNewGame_Click);
+            // 
+            // buttonLoadGame
+            // 
+            this.buttonLoadGame.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonLoadGame.Location = new System.Drawing.Point(93, 226);
+            this.buttonLoadGame.Name = "buttonLoadGame";
+            this.buttonLoadGame.Size = new System.Drawing.Size(75, 23);
+            this.buttonLoadGame.TabIndex = 5;
+            this.buttonLoadGame.Text = "Загрузить";
+            this.buttonLoadGame.UseVisualStyleBackColor = true;
+            // 
+            // buttonSaveGame
+            // 
+            this.buttonSaveGame.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonSaveGame.Location = new System.Drawing.Point(174, 226);
+            this.buttonSaveGame.Name = "buttonSaveGame";
+            this.buttonSaveGame.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveGame.TabIndex = 6;
+            this.buttonSaveGame.Text = "Сохранить";
+            this.buttonSaveGame.UseVisualStyleBackColor = true;
             // 
             // FormSingle
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(340, 261);
+            this.Controls.Add(this.buttonSaveGame);
+            this.Controls.Add(this.buttonLoadGame);
+            this.Controls.Add(this.buttonNewGame);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonSettings);
+            this.Controls.Add(this.labelCurrentTurn);
             this.Controls.Add(this.label1);
             this.Name = "FormSingle";
             this.Text = "Одиночная игра";
             this.Load += new System.EventHandler(this.FormSingle_Load);
+            this.ResizeEnd += new System.EventHandler(this.FormSingle_ResizeEnd);
+            this.SizeChanged += new System.EventHandler(this.FormSingle_ResizeEnd);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormSingle_Paint);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
@@ -92,8 +136,11 @@
 
         #endregion
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelCurrentTurn;
+        private System.Windows.Forms.Button buttonSettings;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonNewGame;
+        private System.Windows.Forms.Button buttonLoadGame;
+        private System.Windows.Forms.Button buttonSaveGame;
     }
 }
--- FormSingle.cs
+++ FormSingle.cs
@@ -11,7 +11,8 @@ using System.Windows.Forms;
 /*
  * Add settings +
  * Replace panel by picturebox +
- * Incorrect turn
+ * Incorrect turn +
+ * Field Scalling
  * Comments
  * Make settings
  * Replace grid by tic tac toe fields
@@ -25,6 +26,7 @@ namespace TTTM
         Position IncorrectTurn;
         Graphics graphics;
         Pen penc1, penc2;
+        string pl1, pl2;
 
         public FormSingle()
         {
@@ -34,23 +36,22 @@ namespace TTTM
 
         private void FormSingle_Load(object sender, EventArgs e)
         {
-            StartSinlgeGame frm = new StartSinlgeGame();
-            if (frm.ShowDialog() != DialogResult.OK)
-            {
-                Close();
-                return;
-            }
-            string pl1, pl2;
-            pl1 = frm.textBox1.Text;
-            pl2 = frm.textBox2.Text;
-            penc1 = new Pen(frm.panel1.BackColor);
-            penc2 = new Pen(frm.panel2.BackColor);
-            game = new SinglePlayerWithFriend(pl1, pl2);
-            label2.Text = pl1;
-            game.ChangeTurn += (s, ev) => { RedrawGame(); if (label2.Text == pl1) label2.Text = pl2; else label2.Text = pl1; };
-            game.IncorrectTurn += Game_IncorrectTurn;
+            
         }
-
+        /*
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0112) // WM_SYSCOMMAND
+            {
+                // Check your window state here
+                if (m.WParam == new IntPtr(0xF030)) // Maximize event - SC_MAXIMIZE from Winuser.h
+                {
+                    FormSingle_ResizeEnd(null, null);
+                }
+            }
+            base.WndProc(ref m);
+        }
+        */
         private void Game_IncorrectTurn(object sender, Position e)
         {
             if (e != null)
@@ -72,23 +73,60 @@ namespace TTTM
 
         private void RedrawGame()
         {
-            Rectangle rectangle = new Rectangle(0, 0, this.Width - 17, this.Height - 40);
-            //graphics.DrawLine(Pens.Blue, new Point(rectangle.Width / 3, 10), new Point(rectangle.Width / 3, rectangle.Height - 10));
-            //graphics.DrawLine(Pens.Blue, new Point(rectangle.Width *2 / 3, 10), new Point(rectangle.Width*2 / 3, rectangle.Height - 10));
-            //graphics.DrawEllipse(System.Drawing.Pens.Black, rectangle);
-            //graphics.DrawRectangle(System.Drawing.Pens.Red, rectangle);
+            //Rectangle rectangle = new Rectangle(0, 0, this.Width - 17, this.Height - 40);
+            float w = pictureBox1.Width;
+            float h = pictureBox1.Height;
 
-            //graphics.FillRectangle(Brushes.Black, rectangle);
+            graphics.DrawLine(Pens.Blue, (float)(1 + 3)/11 * w, (float)1/11 * h, (float)(1+3)/11 * w, (float)10/11 * h);
+            graphics.DrawLine(Pens.Blue, (float)(1 + 3 + 3)/11 * w, (float)1/11 * h, (float)(1+6)/11 * w, (float)10/11 * h);
+            graphics.DrawLine(Pens.Blue, (float)1/11 * w, (float)4/11 * h, (float)10/11 * w, (float)4/11 * h);
+            graphics.DrawLine(Pens.Blue, (float)1/11 * w, (float)7/11 * h, (float)10/11 * w, (float)7/11 * h);
 
-            for (int i = 0; i <= 9; i++)
-            {
-                graphics.DrawLine(Pens.Blue, new Point(i * 20, 0), new Point(i * 20, 20 * 9));
-                graphics.DrawLine(Pens.Blue, new Point(0, i * 20), new Point(20 * 9, i * 20));
-            }
-            
+            Rectangle[,] Zones = new Rectangle[9, 9];
+            for (int i = 0; i < 3; i++)
+                for (int ii = 0; ii < 3; ii++)
+                    for (int j = 0; j < 3; j++)
+                        for (int jj = 0; jj < 3; jj++)
+                        {
+                            double BIGx = (1 + 3 * i) / 11.0;
+                            double SMALLx = (1 + 3 + 3 * ii) / 11.0;
+                            float x = (float)(w * (BIGx + SMALLx * 3 / 11));
 
-            //game.Turn(new Pos(DateTime.Now.Second % 3, DateTime.Now.Millisecond % 3), new Pos(0, 0));
-            //StringBuilder sb = new StringBuilder();
+                            double BIGy = (1 + 3 * j) / 11.0;
+                            double SMALLy = (1 + 3 + 3 * jj) / 11.0;
+                            float y = (float)(h * (BIGy + SMALLy * 3 / 11));
+
+                            double BIGx1 = (1 + 3 * i) / 11.0;
+                            double SMALLx1 = 1.0 / 11;
+                            float x1 = (float)(w * (BIGx1 + SMALLx1 * 3 / 11));
+                            double BIGx2 = (1 + 3 * i) / 11.0;
+                            double SMALLx2 = 10.0 / 11;
+                            float x2 = (float)(w * (BIGx2 + SMALLx2 * 3 / 11));
+
+                            double BIGy1 = (1 + 3 * j) / 11.0;
+                            double SMALLy1 = 1.0 / 11;
+                            float y1 = (float)(h * (BIGy1 + SMALLy1 * 3 / 11));
+                            double BIGy2 = (1 + 3 * j) / 11.0;
+                            double SMALLy2 = 10.0 / 11;
+                            float y2 = (float)(h * (BIGy2 + SMALLy2 * 3 / 11));
+
+                            if (ii != 3 && jj != 3)
+                            {
+                                graphics.DrawLine(Pens.Red, x, y1, x, y2);
+                                graphics.DrawLine(Pens.Red, x1, y, x2, y);
+                            }
+
+                            Zones[i * 3 + ii, j * 3 + jj] = new Rectangle((int)(x-w*9/121), (int)(y-h*9/121), (int)w*9/121, (int)h*9/121);
+                        }
+
+            //for (int i = 0; i <= 9; i++)
+            //{
+            //    graphics.DrawLine(Pens.Blue, new Point(i * 20, 0), new Point(i * 20, 20 * 9));
+            //    graphics.DrawLine(Pens.Blue, new Point(0, i * 20), new Point(20 * 9, i * 20));
+            //}
+            graphics.DrawRectangle(Pens.Lime, Zones[3, 3]);
+            return;
+
             int[,] State = game.State();
             for (int i = 0; i < 9; i++)
             {
@@ -98,13 +136,10 @@ namespace TTTM
                         graphics.DrawEllipse(penc1, i * 20 + 2, j * 20 + 2, 16, 16);
                     if (State[i,j] == 2)
                         graphics.DrawEllipse(penc2, i * 20 + 2, j * 20 + 2, 16, 16);
-                    //sb.Append(State[i,j].ToString());
                 }
-                //sb.Append('\n');
             }
 
             FieldState[,] FState = game.FieldsState();
-            //bool[,] Filled = game.FilledFields();
             for (int i = 0; i < 3; i++)
             {
                 for (int j = 0; j < 3; j++)
@@ -113,12 +148,9 @@ namespace TTTM
                     if (FState[i, j].Filled)
                         graphics.DrawLines(Pens.Gray, DiagonalyLines(new Rectangle(i * 60, j * 60, 60, 60)));
                     if (FState[i, j].OwnerID == 1)
-                        //graphics.DrawEllipse(Pens.Red, j * 60 + 2, i * 60 + 2, 60-4, 60-4);
                         graphics.DrawLines(penc1, DiagonalyLines(new Rectangle(i * 60, j * 60, 60, 60)));
-                        //DiagonalyLines(graphics, new Rectangle(j * 60, i * 60, 60, 60));
                     if (FState[i, j].OwnerID == 2)
                         graphics.DrawLines(penc2, DiagonalyLines(new Rectangle(i * 60, j * 60, 60, 60)));
-                    //graphics.DrawLines(Pens.Green, DiagonalyLines(new Rectangle(j * 60, i * 60, 20, 20)));
                 }
             }
 
@@ -128,13 +160,63 @@ namespace TTTM
 
         private void pictureBox1_Click(object sender, EventArgs e)
         {
-            game.ClickOn((PointToClient(MousePosition).X - pictureBox1.Left) / 20, (PointToClient(MousePosition).Y - pictureBox1.Top) / 20);
+            if (game == null)
+                NewGame();
+            else
+            {
+                game.ClickOn((PointToClient(MousePosition).X - pictureBox1.Left) / 20, (PointToClient(MousePosition).Y - pictureBox1.Top) / 20);
+                RedrawGame();
+                IncorrectTurn = null;
+            }
+        }
+
+        private void NewGame()
+        {
+            StartSinlgeGame frm = new StartSinlgeGame();
+            if (frm.ShowDialog() != DialogResult.OK)
+                return;
+            pl1 = frm.textBox1.Text;
+            pl2 = frm.textBox2.Text;
+            penc1 = new Pen(frm.panel1.BackColor);
+            penc2 = new Pen(frm.panel2.BackColor);
+            game = new SinglePlayerWithFriend(pl1, pl2);
+            labelCurrentTurn.Text = pl1;
+            game.ChangeTurn += Game_ChangeTurn; 
+            game.IncorrectTurn += Game_IncorrectTurn;
+        }
+
+        private void Game_ChangeTurn(object sender, WhosTurn e)
+        {
             RedrawGame();
-            IncorrectTurn = null;
+            if (labelCurrentTurn.Text == pl1)
+                labelCurrentTurn.Text = pl2;
+            else
+                labelCurrentTurn.Text = pl1;
+        }
+
+        private void FormSingle_ResizeEnd(object sender, EventArgs e)
+        {
+            graphics = Graphics.FromHwnd(pictureBox1.Handle);
+            graphics.FillRectangle(Brushes.White, 0, 0, this.Width, this.Height);
+            RedrawGame();
+        }
+
+        private void buttonNewGame_Click(object sender, EventArgs e)
+        {
+            if (game != null)
+            {
+                game.ChangeTurn -= Game_ChangeTurn;
+                game.IncorrectTurn -= Game_IncorrectTurn;
+                game = null;
+            }
+            NewGame();
         }
 
         private void FormSingle_Paint(object sender, PaintEventArgs e)
         {
+            if (game == null)
+                return;
+
             graphics.FillRectangle(Brushes.White, 0, 0, this.Width, this.Height);
             RedrawGame();
         }
