5b289bfe5441cc9cedb059fde6e8247b8be6b5ee
Quantum0 <quantum0@mail.ru>
2016-08-16 03:31:41 +0300

    Возвращён старый вариант отображения

--- FormSingle.cs
+++ FormSingle.cs
@@ -44,97 +44,103 @@ namespace TTTM
                 IncorrectTurn = e;
         }
 
-        private PointF[] DiagonalyLines(Rectangle Rect)
+        private PointF[] DiagonalyLines(Rectangle Rect) // FIX IT!1
         {
             List<PointF> l = new List<PointF>();
+            // x + w - min(w,i) = x + w + max(-w,-i) = x + max(0,w-i)
             for (int i = 0; i <= Math.Max(Rect.Width, Rect.Height); i+=8)
-            { // x + w - min(w,i) = x + w + max(-w,-i) = x + max(0,w-i)
+            { 
                 l.Add(new PointF(Rect.X + Math.Min(Rect.Width, i), Rect.Y));
                 l.Add(new PointF(Rect.X, Rect.Y + Math.Min(Rect.Height, i)));
-                l.Add(new PointF(Rect.X + Math.Max(0, Rect.Width - i), Rect.Y + Rect.Height));
-                l.Add(new PointF(Rect.X + Rect.Width, Rect.Y + Math.Max(0, Rect.Height - i)));
+                //l.Add(new PointF(Rect.X + Math.Max(0, Rect.Width - i), Rect.Y + Rect.Height));
+                //l.Add(new PointF(Rect.X + Rect.Width, Rect.Y + Math.Max(0, Rect.Height - i)));
             }
             return l.ToArray();
         }
 
         private void RedrawGame(bool refreshGraphics = false)
         {
+            // Обновление графики (нужно при ресайзе)
             if (refreshGraphics)
             {
-                BufGFX = context.Allocate(Graphics.FromHwnd(pictureBox1.Handle),
-               new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
+                BufGFX = context.Allocate(Graphics.FromHwnd(pictureBox1.Handle), new Rectangle(new Point(), pictureBox1.Size));
                 gfx = BufGFX.Graphics;
             }
 
+            // Выход при попытке редравнуть несуществующую игру (чтоб избежать эксепшна)
             if (game == null)
                 return;
 
-            gfx.FillRectangle(Brushes.White, 0, 0, this.Width, this.Height);
-
-            //Rectangle rectangle = new Rectangle(0, 0, this.Width - 17, this.Height - 40);
+            // Ширина Высота
             float w = pictureBox1.Width;
             float h = pictureBox1.Height;
 
-            gfx.DrawLine(Pens.Blue, (float)(1 + 3)/11 * w, (float)1/11 * h, (float)(1+3)/11 * w, (float)10/11 * h);
-            gfx.DrawLine(Pens.Blue, (float)(1 + 3 + 3)/11 * w, (float)1/11 * h, (float)(1+6)/11 * w, (float)10/11 * h);
-            gfx.DrawLine(Pens.Blue, (float)1/11 * w, (float)4/11 * h, (float)10/11 * w, (float)4/11 * h);
-            gfx.DrawLine(Pens.Blue, (float)1/11 * w, (float)7/11 * h, (float)10/11 * w, (float)7/11 * h);
+            // Фон
+            gfx.FillRectangle(Brushes.White, 0, 0, w, h);
 
-            for (int i = 0; i < 3; i++)
-                for (int j = 0; j < 3; j++)
-                {
-                    for (int ii = 0; ii < 3; ii++)
-                        for (int jj = 0; jj < 3; jj++)
-                        {
-                            double BIGx = (1 + 3 * i) / 11.0;
-                            double SMALLx = (1 + 3 + 3 * ii) / 11.0;
-                            float x = (float)(w * (BIGx + SMALLx * 3 / 11));
+            /* Не оч выглядит 
+            //gfx.DrawLine(Pens.Blue, (float)(1 + 3)/11 * w, (float)1/11 * h, (float)(1+3)/11 * w, (float)10/11 * h);
+            //gfx.DrawLine(Pens.Blue, (float)(1 + 3 + 3)/11 * w, (float)1/11 * h, (float)(1+6)/11 * w, (float)10/11 * h);
+            //gfx.DrawLine(Pens.Blue, (float)1/11 * w, (float)4/11 * h, (float)10/11 * w, (float)4/11 * h);
+            //gfx.DrawLine(Pens.Blue, (float)1/11 * w, (float)7/11 * h, (float)10/11 * w, (float)7/11 * h);
 
-                            double BIGy = (1 + 3 * j) / 11.0;
-                            double SMALLy = (1 + 3 + 3 * jj) / 11.0;
-                            float y = (float)(h * (BIGy + SMALLy * 3 / 11));
+            //for (int i = 0; i < 3; i++)
+            //    for (int j = 0; j < 3; j++)
+            //    {
+            //        for (int ii = 0; ii < 3; ii++)
+            //            for (int jj = 0; jj < 3; jj++)
+            //            {
+            //                double BIGx = (1 + 3 * i) / 11.0;
+            //                double SMALLx = (1 + 3 + 3 * ii) / 11.0;
+            //                float x = (float)(w * (BIGx + SMALLx * 3 / 11));
 
-                            double BIGx1 = (1 + 3 * i) / 11.0;
-                            double SMALLx1 = 1.0 / 11;
-                            float x1 = (float)(w * (BIGx1 + SMALLx1 * 3 / 11));
-                            double BIGx2 = (1 + 3 * i) / 11.0;
-                            double SMALLx2 = 10.0 / 11;
-                            float x2 = (float)(w * (BIGx2 + SMALLx2 * 3 / 11));
+            //                double BIGy = (1 + 3 * j) / 11.0;
+            //                double SMALLy = (1 + 3 + 3 * jj) / 11.0;
+            //                float y = (float)(h * (BIGy + SMALLy * 3 / 11));
 
-                            double BIGy1 = (1 + 3 * j) / 11.0;
-                            double SMALLy1 = 1.0 / 11;
-                            float y1 = (float)(h * (BIGy1 + SMALLy1 * 3 / 11));
-                            double BIGy2 = (1 + 3 * j) / 11.0;
-                            double SMALLy2 = 10.0 / 11;
-                            float y2 = (float)(h * (BIGy2 + SMALLy2 * 3 / 11));
+            //                double BIGx1 = (1 + 3 * i) / 11.0;
+            //                double SMALLx1 = 1.0 / 11;
+            //                float x1 = (float)(w * (BIGx1 + SMALLx1 * 3 / 11));
+            //                double BIGx2 = (1 + 3 * i) / 11.0;
+            //                double SMALLx2 = 10.0 / 11;
+            //                float x2 = (float)(w * (BIGx2 + SMALLx2 * 3 / 11));
 
-                            if (ii != 2 && jj != 2)
-                            {
-                                gfx.DrawLine(Pens.Red, x, y1, x, y2);
-                                gfx.DrawLine(Pens.Red, x1, y, x2, y);
-                            }
+            //                double BIGy1 = (1 + 3 * j) / 11.0;
+            //                double SMALLy1 = 1.0 / 11;
+            //                float y1 = (float)(h * (BIGy1 + SMALLy1 * 3 / 11));
+            //                double BIGy2 = (1 + 3 * j) / 11.0;
+            //                double SMALLy2 = 10.0 / 11;
+            //                float y2 = (float)(h * (BIGy2 + SMALLy2 * 3 / 11));
 
-                            Zones[i * 3 + ii, j * 3 + jj] = new Rectangle((int)(x - w * 9 / 121), (int)(y - h * 9 / 121), (int)w * 9 / 121, (int)h * 9 / 121);
-                        }
-                    FieldZones[i, j] = new Rectangle((int)w * (1 + 3 * i) / 11, (int)h * (1 + 3 * j) / 11, (int)w * 3 / 11, (int)h * 3 / 11);
-                }
+            //                if (ii != 2 && jj != 2)
+            //                {
+            //                    gfx.DrawLine(Pens.Red, x, y1, x, y2);
+            //                    gfx.DrawLine(Pens.Red, x1, y, x2, y);
+            //                }
 
-            //for (int i = 0; i <= 9; i++)
-            //{
-            //    graphics.DrawLine(Pens.Blue, new Point(i * 20, 0), new Point(i * 20, 20 * 9));
-            //    graphics.DrawLine(Pens.Blue, new Point(0, i * 20), new Point(20 * 9, i * 20));
-            //}
-            //graphics.DrawRectangle(Pens.Lime, Zones[3, 3]);
+            //                Zones[i * 3 + ii, j * 3 + jj] = new Rectangle((int)(x - w * 9 / 121), (int)(y - h * 9 / 121), (int)w * 9 / 121, (int)h * 9 / 121);
+            //            }
+            //        FieldZones[i, j] = new Rectangle((int)w * (1 + 3 * i) / 11, (int)h * (1 + 3 * j) / 11, (int)w * 3 / 11, (int)h * 3 / 11);
+            //    }
 
+            */
+
+            for (int i = 1; i <= 10; i++)
+            {
+                gfx.DrawLine(Pens.Blue, new PointF(w * i / 11f, h / 11f), new PointF(w * i / 11f, h * 10 / 11f));
+                gfx.DrawLine(Pens.Blue, new PointF(w / 11f, h * i / 11f), new PointF(w * 10 / 11f, h * i / 11f));
+            }
+            
             int[,] State = game.State();
             for (int i = 0; i < 9; i++)
             {
                 for (int j = 0; j < 9; j++)
                 {
+                    Rectangle rect = new Rectangle((int)((w * (i + 1)) / 11f), (int)((h * (j + 1)) / 11f), (int)(w / 11f), (int)(h / 11f));
                     if (State[i,j] == 1)
-                        gfx.DrawEllipse(penc1, Zones[i, j]);
+                        gfx.DrawEllipse(penc1, rect);
                     if (State[i,j] == 2)
-                        gfx.DrawEllipse(penc2, Zones[i, j]);
+                        gfx.DrawEllipse(penc2, rect);
                 }
             }
 
@@ -143,18 +149,19 @@ namespace TTTM
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    gfx.DrawRectangle(Pens.Orange, FieldZones[i, j]);
+                    Rectangle rect = new Rectangle((int)((w * (i * 3 + 1)) / 11f), (int)((h * (j * 3 + 1)) / 11f), (int)(3 * w / 11f), (int)(3 * h / 11f));
+                    gfx.DrawRectangle(Pens.Orange, rect);
                     if (FState[i, j].Filled)
-                        gfx.DrawLines(Pens.Gray, DiagonalyLines(FieldZones[i, j]));
+                        gfx.DrawLines(Pens.Gray, DiagonalyLines(rect));
                     if (FState[i, j].OwnerID == 1)
-                        gfx.DrawLines(penc1, DiagonalyLines(FieldZones[i, j]));
+                        gfx.DrawLines(penc1, DiagonalyLines(rect));
                     if (FState[i, j].OwnerID == 2)
-                        gfx.DrawLines(penc2, DiagonalyLines(FieldZones[i, j]));
+                        gfx.DrawLines(penc2, DiagonalyLines(rect));
                 }
             }
 
             if (IncorrectTurn != null)
-                gfx.DrawRectangle(Pens.Yellow, FieldZones[IncorrectTurn.x, IncorrectTurn.y]);
+                gfx.DrawRectangle(Pens.Yellow, new Rectangle((int)((w * (IncorrectTurn.x * 3 + 1)) / 11f), (int)((h * (IncorrectTurn.y * 3 + 1)) / 11f), (int)(w * 3/ 11f), (int)(h * 3/ 11f)));
 
             BufGFX.Render();
         }
@@ -166,14 +173,8 @@ namespace TTTM
             else
             {
                 Point pnt = new Point(PointToClient(MousePosition).X - pictureBox1.Left, PointToClient(MousePosition).Y - pictureBox1.Top);
-                for (int i = 0; i < 3; i++)
-                {
-                    for (int j = 0; j < 3; j++)
-                    {
-                        if (Zones[i, j].Contains(pnt))
-                            game.ClickOn(i, j);
-                    }
-                }
+                PointF pntf = new PointF(pnt.X * 11f / pictureBox1.Width - 1, pnt.Y * 11f / pictureBox1.Height - 1);
+                game.ClickOn((int)pntf.X, (int)pntf.Y);
                 //game.ClickOn((PointToClient(MousePosition).X - pictureBox1.Left) / 20, (PointToClient(MousePosition).Y - pictureBox1.Top) / 20);
                 RedrawGame();
                 IncorrectTurn = null;
@@ -228,7 +229,7 @@ namespace TTTM
 
         private void FormSingle_Paint(object sender, PaintEventArgs e)
         {
-            RedrawGame();
+            //RedrawGame();
         }
     }
 }
